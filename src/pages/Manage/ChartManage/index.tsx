24aa4cc34b5976f78f802c6fd51905861589c211
nanchengcyu <1146137024@qq.com>
2024-04-26 17:17:37 +0800

    我的图表页面增加异步生成中效果，并修改页面美观度

--- src/pages/Manage/ChartManage/index.tsx
+++ src/pages/Manage/ChartManage/index.tsx
@@ -1,15 +1,19 @@
+import {globalData} from '@/global';
 import {
   deleteChartUsingPOST,
   listMyChartByPageUsingPOST,
 } from '@/services/nanchengyubi/ChartController';
-import { Link, useModel } from '@@/exports';
-import { ExclamationCircleOutlined } from '@ant-design/icons';
-import { Avatar, Button, Card, Divider, List, message, Modal } from 'antd';
+import {Link, useModel} from '@@/exports';
+import {ExclamationCircleOutlined} from '@ant-design/icons';
+import {Avatar, Button, Card, Col, Divider, List, message, Modal, Result, Row} from 'antd';
 import Search from 'antd/es/input/Search';
 import ReactECharts from 'echarts-for-react';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 const MyChartPage: React.FC = () => {
+  /**
+   * 初始值
+   */
   const initSearchParams = {
     current: 1,
     pageSize: 4,
@@ -17,65 +21,103 @@ const MyChartPage: React.FC = () => {
     sortOrder: 'desc',
   };
 
+  // 设置全局变量
+  const [isGlobalEnabled, setIsGlobalEnabled] = useState(globalData.isGlobalEnabled);
+  // 更新全局变量
+  useEffect(() => {
+    setIsGlobalEnabled(globalData.isGlobalEnabled);
+  }, []);
+
+  // 使用 useRef 创建定时器的引用
+  const refreshTimer = useRef(null);
+
+  /**
+   * 查询参数
+   */
   const [searchParams, setSearchParams] = useState<API.ChartQueryRequest>({
     ...initSearchParams,
   });
 
+  /**
+   * 分页获取图表
+   */
   const [chartList, setChartList] = useState<API.Chart[]>();
   const [chartTotal, setChartTotal] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(true);
 
-  const { initialState } = useModel('@@initialState');
-  const { currentUser } = initialState || {};
-  const isValidJson = (jsonString: string) => {
-    try {
-      JSON.parse(jsonString);
-      return true;
-    } catch (error) {
-      return false;
-    }
-  };
+  /**
+   * 获取当前用户
+   */
+  const {initialState} = useModel('@@initialState');
+  const {currentUser} = initialState || {};
 
+  /**
+   * 加载图表数据
+   */
   const loadData = async () => {
-    setLoading(loading);
+    setLoading(true);
     try {
       let res = await listMyChartByPageUsingPOST(searchParams);
       if (res.data) {
         setChartList(res.data.records ?? []);
         setChartTotal(res.data.total ?? 0);
+        // 隐藏title
         if (res.data.records) {
+          let shouldRefresh = false;
           res.data.records.forEach((data) => {
             if (data.chartStatus === 'succeed') {
               const chartOption = JSON.parse(data.genChart ?? '{}');
               chartOption.title = undefined;
               data.genChart = JSON.stringify(chartOption);
+            } else {
+              shouldRefresh = true;
             }
           });
+
+          if (!shouldRefresh) {
+            clearInterval(refreshTimer.current);
+          }
         }
       } else {
-        message.error('获取我的图表失败');
+        message.error('获取图表失败');
       }
     } catch (e: any) {
-      message.error('获取我的图表失败' + e.message);
+      message.error('获取图表失败' + e.message);
     }
     setLoading(false);
   };
 
+  // 变化时执行此处，从后端加载数据
   useEffect(() => {
     loadData();
   }, [searchParams]);
 
+  // 每 10 秒钟自动刷新一次
+  useEffect(() => {
+    refreshTimer.current = setInterval(() => {
+      if (isGlobalEnabled) {
+        loadData();
+      }
+    }, 10000);
+    return () => {
+      clearInterval(refreshTimer.current);
+    };
+  }, [isGlobalEnabled]);
+
+  /**
+   * 删除图表
+   * @param chartId
+   */
   const handleDelete = (chartId: any) => {
     Modal.confirm({
       title: '确认删除',
-      icon: <ExclamationCircleOutlined />,
+      icon: <ExclamationCircleOutlined/>,
       content: '确定要删除这个图表吗？',
       okText: '确认',
       cancelText: '取消',
       onOk: async () => {
         try {
-          const res = await deleteChartUsingPOST({ id: chartId });
-          console.log('res:', res.data);
+          const res = await deleteChartUsingPOST({id: chartId});
           if (res.data) {
             message.success('删除成功');
             loadData();
@@ -89,7 +131,6 @@ const MyChartPage: React.FC = () => {
     });
   };
 
-  // @ts-ignore
   return (
     <div className="my-chart-page">
       <div className="margin-20">
@@ -136,59 +177,120 @@ const MyChartPage: React.FC = () => {
         dataSource={chartList}
         renderItem={(item) => (
           <List.Item key={item.id}>
-            <div style={{ overflow: 'auto', maxHeight: '500px' }}>
-              <Card style={{ width: '100%', maxWidth: '800px' }}>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <List.Item.Meta
-                    avatar={<Avatar src={currentUser?.userAvatar} />}
-                    title={currentUser?.userName}
-                  description={item.chartType ? '图表类型：' + item.chartType : undefined}
-                  />
-
-                  <div>
-                    <Link to={`/ViewChartData/${item.id}`}>
-                      <Button style={{ backgroundColor: '#007BFF', color: 'white' }}>查看图表数据</Button>
-                    </Link>
-                    <span style={{ margin: '0 10px' }}></span>
-                    <Button danger onClick={() => handleDelete(item.id)}>
-                      删除
-                    </Button>
-                  </div>
-
-                </div>
-                {item.chartStatus === 'succeed' && (
+            <Card style={{width: '100%'}}>
+              <List.Item.Meta
+                avatar={<Avatar src={currentUser?.userAvatar}/>}
+                title={currentUser?.userName}
+              />
+              <>
+                {item.chartStatus === 'wait' && (
                   <>
-
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                      <div style={{ background: '#007BFF', padding: '5px 10px', borderRadius: '4px', display: 'inline-block' }}>
-                        <Divider style={{ fontSize: '15px', fontWeight: 'bold', color: 'white', margin: 0 }}>{'图表标题：' + item.goal}</Divider>
-                      </div>
-                    </div>
-                    <ReactECharts
-                      option={item.genChart && isValidJson(item.genChart) ? JSON.parse(item.genChart) : null}
+                    <Result
+                      status="warning"
+                      title="排队中...."
+                      subTitle={item.execMessage ?? '系统繁忙，请稍后重试'}
                     />
-
-                    <p style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '16px',color:'#007BFF' }}>
-                      {'图表名称：' + item.chartName}
-                    </p>
-
-                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                      <div style={{ fontSize: '14px', color: 'gray' }}>
-                        {'图表生成时间：' + (item.createTime ? new Date(item.createTime).toLocaleString() : '未知时间')}
-                      </div>
-                    </div>
-
-
+                    <Row>
+                      <Col push={17}>
+                        <Button danger onClick={() => handleDelete(item.id)}>
+                          删除
+                        </Button>
+                      </Col>
+                    </Row>
                   </>
                 )}
-              </Card>
-            </div>
+                {item.chartStatus === 'running' && (
+                  <>
+                    <Result status="info" title="图表生成中...." subTitle={item.execMessage}/>
+                  </>
+                )}
+                {item.chartStatus === 'succeed' && (
+                  <>
+                    <p
+                      style={{
+                        textAlign: 'center',
+                        fontWeight: 'bold',
+                        color: '#0e5a94',
+                        fontSize: '18px',
+                        marginBottom: '15px',
+                        marginTop: '10px',
+                        borderBottom: '2px solid #007bff',
+                      }}
+                    >
+                      {'图表标题：' + item.goal}
+                    </p>
+                    <List.Item.Meta
+                      style={{textAlign: 'left', fontWeight: 'bold'}}
+                      description={item.chartType ? '图表类型：' + item.chartType : undefined}
+                    />
+                    <ReactECharts option={item.genChart && JSON.parse(item.genChart)}/>
+                    <p
+                      style={{
+                        textAlign: 'center',
+                        fontWeight: 'bold',
+                        color: '#0e5a94',
+                        fontSize: '16px',
+                      }}
+                    >
+                      {'图表名称：' + item.chartName}
+                    </p>
+                    <Divider
+                      style={{
+                        fontWeight: 'bold',
+                        color: '#333', // 文字颜色保持为深色调以增加可读性
+                        fontSize: '16px',
+                        marginBottom: '10px',
+                        marginTop: '10px',
+                        borderColor: '#5885e6', // 设置分隔线的颜色为更优雅的蓝色调
+                      }}
+                    >
+                      智能分析结果
+                    </Divider>
+                    <div style={{whiteSpace: 'pre-wrap', overflow: 'auto'}}>
+                      <p style={{color: 'black', textAlign: 'left'}}>
+                        {item.genResult}
+                      </p>
+                    </div>
+                    <Row>
+                      <Col style={{ color: 'black' }}>
+                        {'图表生成时间：' + (item.createTime ? new Date(item.createTime).toLocaleString() : '未知时间')}
+                      </Col>
+                      {/* 使用 Col 组件来确保按钮并排展示，去掉了 push 属性 */}
+                      <Col span={12} style={{ textAlign: 'right' }}>
+                        <Link to={`/ViewChartData/${item.id}`}>
+                          <Button type="primary">查看图表数据</Button>
+                          </Link>
+                          <Button danger style={{ marginLeft: '10px' }} onClick={() => handleDelete(item.id)}>
+                          删除
+                        </Button>
+                      </Col>
+                    </Row>
+                  </>
+                )}
+                {item.chartStatus === 'failed' && (
+                  <>
+                    <Result status="error" title="图表生成失败" subTitle={item.execMessage}/>
+                    <Row justify="end">
+                      <Col style={{paddingRight: '10px'}}>
+                        <Button type="primary" onClick={() => message.warning('敬请期待')}>
+                          重试
+                        </Button>
+                      </Col>
+                      <Col>
+                        <Button danger onClick={() => handleDelete(item.id)}>
+                          删除
+                        </Button>
+                      </Col>
+                    </Row>
+                  </>
+                )}
+              </>
+            </Card>
           </List.Item>
         )}
       />
     </div>
   );
-
 };
 
 export default MyChartPage;
